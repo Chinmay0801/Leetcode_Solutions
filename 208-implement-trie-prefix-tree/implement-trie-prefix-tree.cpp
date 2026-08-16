06cde52ca6f9e6ffab6ab97805773ef7493b66ab
Chinmay Deshpande <Chinmaydeshapande@gmail.com>
2026-08-16 17:30:38 +0530

    Time: 11 ms (95.87%) | Memory: 50.4 MB (78.85%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,76 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode() {
+        isEnd = false;
+
+        for(int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+
+        TrieNode* curr = root;
+
+        for(char c : word) {
+
+            int index = c - 'a';
+
+            if(curr->children[index] == nullptr) {
+                curr->children[index] = new TrieNode();
+            }
+
+            curr = curr->children[index];
+        }
+
+        curr->isEnd = true;
+    }
+
+    bool search(string word) {
+
+        TrieNode* curr = root;
+
+        for(char c : word) {
+
+            int index = c - 'a';
+
+            if(curr->children[index] == nullptr) {
+                return false;
+            }
+
+            curr = curr->children[index];
+        }
+
+        return curr->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+
+        TrieNode* curr = root;
+
+        for(char c : prefix) {
+
+            int index = c - 'a';
+
+            if(curr->children[index] == nullptr) {
+                return false;
+            }
+
+            curr = curr->children[index];
+        }
+
+        return true;
+    }
+};
